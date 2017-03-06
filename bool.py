39f4b4543148a61a0d1acaffee2ab5f50ccdcff9
palashgpt22 <palashgpt22@gmail.com>
2017-03-06 21:26:04 +0530

    boolean api

--- bool.py
+++ bool.py
@@ -1,11 +1,28 @@
 import os
 from flask import Flask
-
+from flask import request
 app = Flask(__name__)
+
+param = None
 
 @app.route("/")
 def hello():
-    return "Hello world!"
+    return "the current value of your boolean is " + str(param)
+
+
+@app.route("/setvalue")
+def setvalue():
+    global param
+    userval = request.args.get('value')
+    if (userval == 'true' or userval == 'True' or userval == '1'):
+        param = True
+    elif (userval == 'false' or userval == 'False' or userval == '0'):
+        param = False
+    else:
+        return "please provide a valid value: True for true or True or 1 and False for false False or 0"
+
+
+    return "the new value of your boolean is " + str(param)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
